a4bd9d1b012dac9bbd06486d5e7849bf5e5c4796
Sam <splavigne@gmail.com>
2017-10-31 14:30:54 -0400

    ip camera example

---
+++ examples/face_detect_ip_camera.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+import ipcamera
+import time
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+video_capture = cv2.VideoCapture(ipcamera.MJPG_URL)
+
+# get the current time in seconds
+last_moved = time.time()
+
+recording = 0
+
+while True:
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # find the biggest face and center the camera on it
+    current_time = time.time()
+    if len(faces) > 0 and current_time - last_moved > 4:
+        biggest_face = sorted(faces, key=lambda x: x[2])[0]
+        ipcamera.record(str(recording) + '.mp4', duration='00:00:03')
+        ipcamera.center(biggest_face[0] + biggest_face[2]/2, biggest_face[1] + biggest_face[3]/2)
+        last_moved = time.time()
+        recording += 1
+
+
+    # comment out these lines if you don't want to see a preview window
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ examples/ipcamera.py
@@ -0,0 +1,71 @@
+'''
+Control an axis IP camera with python
+Manual can be found at: https://www.axis.com/files/manuals/um_p5534_45175_en_1112.pdf
+Pan/tilt/zoom api (see page 20): https://www.axis.com/files/manuals/vapix_ptz_52933_en_1307.pdf
+'''
+
+import requests
+import subprocess
+
+BASE_URL = 'http://172.22.151.17'
+CONTROL_URL = BASE_URL + '/axis-cgi/com/ptz.cgi'
+MJPG_URL = BASE_URL + '/mjpg/video.mjpg'
+RTSP_URL = (BASE_URL + ':554/axis-media/media.amp').replace('http://', 'rtsp://')
+
+USER = 'root'
+PASS = 'enter'
+
+
+def relative_zoom(steps):
+    '''
+    Zooms the camera in or out relative to current zoom
+    positive value zooms in, negative zooms out
+    '''
+    requests.get(CONTROL_URL, {'rzoom': steps})
+
+
+def zoom(steps):
+    '''
+    Zooms to specific location between 1 and 9999
+    '''
+    requests.get(CONTROL_URL, {'zoom': steps})
+
+
+def pan(location):
+    '''Pans to a specific location between -180 and 180'''
+    requests.get(CONTROL_URL, {'pan': location})
+
+
+def tilt(location):
+    '''Tilts to a specific location between -180 and 180'''
+    requests.get(CONTROL_URL, {'tilt': location})
+
+
+def center(x, y):
+    '''Centers the camera on an x,y coordinate'''
+    requests.get(CONTROL_URL, {'center': '{},{}'.format(x, y)})
+
+
+def move(location):
+    '''
+    Moves the image 25 % of the image field width in the specified direction.
+    '''
+    valid = ['home', 'up', 'down', 'left', 'right',
+             'upleft', 'upright', 'downleft', 'downright', 'stop']
+    if location in valid:
+        requests.get(CONTROL_URL, {'move': location})
+
+
+def record(outputname, duration='10:00:00', rtsp=True):
+    '''
+    Records a video from the camera
+    Default duration is for 10 hours
+    You can either use rtsp or mjpeg
+    '''
+
+    if rtsp:
+        args = ['ffmpeg', '-i', RTSP_URL, '-vcodec', 'copy', '-acodec', 'copy', '-f', 'mp4', '-t', duration, '-y', outputname]
+    else:
+        args = ['ffmpeg', '-f', 'mjpeg', '-r', '30', '-i', MJPG_URL, '-t', duration, '-y', outputname]
+
+    subprocess.Popen(args)
